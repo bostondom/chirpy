bddcea9836633c499d64ad765350e6407e62fb0e
bostondom <bostondom@gmail.com>
2025-02-10 14:18:12 -0500

    healthz, metrics GET; reset POST

--- main.go
+++ main.go
@@ -3,15 +3,48 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(){
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+
+	mux.HandleFunc("GET /metrics",func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hits: %d\n", apiCfg.fileserverHits.Load())
+	})
+
+	mux.HandleFunc("POST /reset",func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.middlewareMetricsReset()
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
 
 	srv := &http.Server{
 		Addr:    ":" + port,
