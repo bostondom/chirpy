78418e7b3f9fe887362b9657751ceb120f6b4033
bostondom <bostondom@gmail.com>
2025-02-10 14:27:42 -0500

    make separate file and funcs

--- main.go
+++ main.go
@@ -4,19 +4,18 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
-	"fmt"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w,r)
-	})
-}
+// func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)  {
+// 		cfg.fileserverHits.Add(1)
+// 		next.ServeHTTP(w,r)
+// 	})
+// }
 
 func (cfg *apiConfig) middlewareMetricsReset(){
 	cfg.fileserverHits.Store(0)
@@ -29,22 +28,12 @@ func main() {
 	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 
 	mux.HandleFunc("GET /healthz", handlerReadiness)
-
-	mux.HandleFunc("GET /metrics",func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Hits: %d\n", apiCfg.fileserverHits.Load())
-	})
-
-	mux.HandleFunc("POST /reset",func(w http.ResponseWriter, r *http.Request) {
-		apiCfg.middlewareMetricsReset()
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(http.StatusText(http.StatusOK)))
-	})
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -55,8 +44,8 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
+// func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write([]byte(http.StatusText(http.StatusOK)))
+// }
